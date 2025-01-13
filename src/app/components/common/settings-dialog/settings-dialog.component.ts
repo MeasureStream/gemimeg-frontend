3f4176b2a40e322e73293d132075f5a36a241260
Samuel Eickelberg <samuel.eickelberg@ptb.de>
2025-01-13 18:00:29 +0100

    Current user language is being set as default for the drop down box, and it gets updated by selecting another language from the list.

--- src/app/components/common/settings-dialog/settings-dialog.component.ts
+++ src/app/components/common/settings-dialog/settings-dialog.component.ts
@@ -10,40 +10,31 @@ import { NavigationComponent } from '../navigation/navigation.component';
 export class SettingsDialogComponent {
   @Output() close = new EventEmitter<void>();
 
-  languages: Array<Language>;
-  language: Language;
+  displaylanguages: Array<Language>;
+  userLanguage: string;
 
   constructor(private navigation: NavigationComponent) {
-    this.languages = new Array<Language>;
-    this.languages[0] = <Language>{};
-    this.languages[0].name = "Deutsch";
-    this.languages[0].value = "de";
-    this.languages[1] = <Language>{};
-    this.languages[1].name = "English";
-    this.languages[1].value = "en";
-    this.languages[2] = <Language>{};
-    this.languages[2].name = "Français";
-    this.languages[2].value = "fr";
-    this.languages[3] = <Language>{};
-    this.languages[3].name = "Español";
-    this.languages[3].value = "es";
-    this.language = this.getLanguage(this.navigation.getUserLanguage());
-  }
-
-  getLanguage(value: string): Language {
-    for (let i = 0; i < this.languages.length; i++) {
-      if (this.languages[i].value == value) {
-        return this.languages[i];
-      }
-    }
-    return this.languages[0];
+    this.displaylanguages = new Array<Language>;
+    this.displaylanguages[0] = <Language>{};
+    this.displaylanguages[0].name = "Deutsch";
+    this.displaylanguages[0].value = "de";
+    this.displaylanguages[1] = <Language>{};
+    this.displaylanguages[1].name = "English";
+    this.displaylanguages[1].value = "en";
+    this.displaylanguages[2] = <Language>{};
+    this.displaylanguages[2].name = "Français";
+    this.displaylanguages[2].value = "fr";
+    this.displaylanguages[3] = <Language>{};
+    this.displaylanguages[3].name = "Español";
+    this.displaylanguages[3].value = "es";
+    this.userLanguage = this.navigation.getUserLanguage();
   }
 
   ngOnInit(): void {
   }
 
   applyLanguage(): void {
-    this.navigation.setUserLanguage(this.language.value);
+    this.navigation.setUserLanguage(this.userLanguage);
     this.closeDialog();
   }
 
