10633f6a1fd27b6855e9daa267f543f5dbfaed9b
Samuel Eickelberg <samuel.eickelberg@ptb.de>
2025-03-27 16:02:00 +0100

    Fixed discrepancies between oplayer-frontend and gemimeg-frontend

--- src/app/components/dcc/dcc-identifications/dcc-identifications.component.ts
+++ src/app/components/dcc/dcc-identifications/dcc-identifications.component.ts
@@ -1,8 +1,37 @@
+/**
+*  Copyright 2025 Physikalisch-Technische Bundesanstalt
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions are met:
+*
+*  1. Redistributions of source code must retain the above copyright notice,
+*  this list of conditions and the following disclaimer.
+*
+*  2. Redistributions in binary form must reproduce the above copyright notice,
+*  this list of conditions and the following disclaimer in the documentation
+*  and/or other materials provided with the distribution.
+*
+*  3. Neither the name of the copyright holder nor the names of its contributors
+*  may be used to endorse or promote products derived from this software without
+*  specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND
+*  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+*  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+*  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+*  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+*  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+*  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+*  OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+
 import { Component, Input, OnInit } from '@angular/core';
 
-import { LanguageSpecificStringsDto } from 'src/app/generated/dcc/model/languageSpecificStringsDto';
 import { IdentificationDto } from 'src/app/generated/dcc/model/identificationDto';
-import { LangTextPair } from 'src/app/generated/dcc/model/langTextPair';
+import { InitializationService } from 'src/app/services/dcc/initialization.service';
 
 @Component({
   selector: 'app-dcc-identifications',
@@ -10,39 +39,30 @@ import { LangTextPair } from 'src/app/generated/dcc/model/langTextPair';
   styleUrls: ['./dcc-identifications.component.scss']
 })
 export class DccIdentificationsComponent implements OnInit {
-
   @Input() list: Array<IdentificationDto>;
+  
   isExpanded:boolean[]=[true];
   validIdentificationIssuers = ["manufacturer", "calibrationLaboratory", "customer", "owner", "other"];
 
-
-  constructor() {
+  constructor(private initializationService: InitializationService) {
     this.list = new Array<IdentificationDto>();
+    this.addEmptyIdentificationDto();
   }
 
   ngOnInit(): void {
-
   }
 
-  getEmptyIdentifictionDto(): IdentificationDto {
-    var result =<IdentificationDto>{}
-    result.issuer="";
-    result.value="";
-    result.name=this.getEmptyLanguageSpecificStringsDto();
-    return result;
-  }
-  getEmptyLanguageSpecificStringsDto(): LanguageSpecificStringsDto {
-    var result = <LanguageSpecificStringsDto>{};
-    result.content = new Array<LangTextPair>;
-    result.content.push(<LangTextPair>{})
-    return result;
-  }
-  toggleCard(index:number){
+  toggleCard(index: number) {
     console.log('toggle: ',index)
     this.isExpanded[index]=!this.isExpanded[index];
     console.log('index: ',this.isExpanded[index])
   }
-  addExpanded(){
+
+  addEmptyIdentificationDto() {
+    this.list.push(this.initializationService.getEmptyIdentifictionDto());
+  }
+
+  addExpanded() {
     this.isExpanded.push(true);
     console.log('isExpanded', this.isExpanded)
   }
