fe41e0c90df6f2202ef178c5a273b100525aac8c
rehman01 <abdul.rehman@ptb.de>
2025-03-27 14:09:32 +0100

    OP#17338: the removal of space between footer-nav and imprint , and the adjustment of 5th element Preview with footer-nav.

--- src/app/components/dcc/dcc.component.ts
+++ src/app/components/dcc/dcc.component.ts
@@ -54,7 +54,7 @@ export class DccComponent implements OnInit, AfterContentChecked {
   selectedPerformanceLoc: string = '';
   performanceLocation = ["laboratory", "customer", "laboratory branch", "customer branch","other"];
   currentStepIndex = 0;
-  totalSteps = 4;
+  totalSteps = 5;
   isLastStep =false
   chosenFileData: ByteDataDto | null = null;
   @ViewChild(DccMeasurementMetadataComponent) metadataComponent!: DccMeasurementMetadataComponent;
@@ -524,7 +524,7 @@ export class DccComponent implements OnInit, AfterContentChecked {
   }
 
   onStepChange(event: StepperSelectionEvent): void {
-    if (event.selectedIndex === 4) {
+    if (event.selectedIndex === 5) {
       this.onTabChange({ index: 0, tab: { textLabel: 'Human Readable' } } as MatTabChangeEvent);
     }
     this.currentStepIndex = event.selectedIndex;
